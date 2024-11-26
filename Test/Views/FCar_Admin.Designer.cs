50fe07fed88b0aa833884dfa8969475b137eb572
hoangDEVteams <ghoang.it.dev@gmail.com>
2024-11-26 14:04:39 +0700

    save change

--- Test/Views/FCar_Admin.Designer.cs
+++ Test/Views/FCar_Admin.Designer.cs
@@ -29,34 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FCar_Admin));
-<<<<<<< HEAD
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnEdit = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtYear = new System.Windows.Forms.RichTextBox();
-            this.txtName = new System.Windows.Forms.RichTextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtManu = new System.Windows.Forms.RichTextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtCode = new System.Windows.Forms.RichTextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.dtgridVehicles = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgridVehicles)).BeginInit();
-            this.panel1.SuspendLayout();
-=======
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox6 = new System.Windows.Forms.GroupBox();
             this.label10 = new System.Windows.Forms.Label();
@@ -104,7 +76,6 @@
             this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
->>>>>>> b7d2c3d3db28c03e14b1e2b6c92b28315ea84070
             this.SuspendLayout();
             // 
             // groupBox1
@@ -136,7 +107,6 @@
             this.groupBox6.Size = new System.Drawing.Size(1306, 346);
             this.groupBox6.TabIndex = 6;
             this.groupBox6.TabStop = false;
-            this.groupBox6.Enter += new System.EventHandler(this.groupBox6_Enter);
             // 
             // label10
             // 
@@ -192,7 +162,6 @@
             this.iconButton6.Text = "Upload Image";
             this.iconButton6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.iconButton6.UseVisualStyleBackColor = false;
-            this.iconButton6.Click += new System.EventHandler(this.iconButton6_Click);
             // 
             // label9
             // 
@@ -556,7 +525,6 @@
             this.iconButton3.Text = "Add";
             this.iconButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.iconButton3.UseVisualStyleBackColor = false;
-            this.iconButton3.Click += new System.EventHandler(this.iconButton3_Click);
             // 
             // iconButton2
             // 
@@ -591,222 +559,6 @@
             // 
             // openFileDialog1
             // 
-<<<<<<< HEAD
-            this.panel2.Controls.Add(this.btnAdd);
-            this.panel2.Controls.Add(this.btnEdit);
-            this.panel2.Controls.Add(this.btnDelete);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.txtYear);
-            this.panel2.Controls.Add(this.txtName);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.txtManu);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.txtCode);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Location = new System.Drawing.Point(1158, 46);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(471, 658);
-            this.panel2.TabIndex = 3;
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.BackColor = System.Drawing.Color.Lime;
-            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.ForeColor = System.Drawing.Color.Black;
-            this.btnAdd.Location = new System.Drawing.Point(327, 596);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(129, 43);
-            this.btnAdd.TabIndex = 19;
-            this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = false;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // btnEdit
-            // 
-            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEdit.ForeColor = System.Drawing.Color.Black;
-            this.btnEdit.Location = new System.Drawing.Point(181, 596);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(129, 43);
-            this.btnEdit.TabIndex = 19;
-            this.btnEdit.Text = "Edit";
-            this.btnEdit.UseVisualStyleBackColor = false;
-            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.BackColor = System.Drawing.Color.Firebrick;
-            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.ForeColor = System.Drawing.Color.White;
-            this.btnDelete.Location = new System.Drawing.Point(22, 596);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(129, 43);
-            this.btnDelete.TabIndex = 19;
-            this.btnDelete.Text = "Delete";
-            this.btnDelete.UseVisualStyleBackColor = false;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(401, 269);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(55, 25);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "...";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(18, 526);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(157, 20);
-            this.label5.TabIndex = 18;
-            this.label5.Text = "ManufactureYear:";
-            // 
-            // txtYear
-            // 
-            this.txtYear.Location = new System.Drawing.Point(181, 516);
-            this.txtYear.Name = "txtYear";
-            this.txtYear.Size = new System.Drawing.Size(275, 30);
-            this.txtYear.TabIndex = 17;
-            this.txtYear.Text = "";
-            // 
-            // txtName
-            // 
-            this.txtName.Location = new System.Drawing.Point(181, 388);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(275, 30);
-            this.txtName.TabIndex = 16;
-            this.txtName.Text = "";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(18, 460);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(126, 20);
-            this.label4.TabIndex = 15;
-            this.label4.Text = "Manufacturer:";
-            // 
-            // txtManu
-            // 
-            this.txtManu.Location = new System.Drawing.Point(181, 450);
-            this.txtManu.Name = "txtManu";
-            this.txtManu.Size = new System.Drawing.Size(275, 30);
-            this.txtManu.TabIndex = 14;
-            this.txtManu.Text = "";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(18, 398);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(131, 20);
-            this.label3.TabIndex = 13;
-            this.label3.Text = "Vehicle Name:";
-            // 
-            // txtCode
-            // 
-            this.txtCode.Location = new System.Drawing.Point(181, 322);
-            this.txtCode.Name = "txtCode";
-            this.txtCode.Size = new System.Drawing.Size(275, 30);
-            this.txtCode.TabIndex = 12;
-            this.txtCode.Text = "";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(18, 332);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(126, 20);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "Vehicle Code:";
-            // 
-            // dtgridVehicles
-            // 
-            this.dtgridVehicles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgridVehicles.Location = new System.Drawing.Point(0, 153);
-            this.dtgridVehicles.Name = "dtgridVehicles";
-            this.dtgridVehicles.RowHeadersWidth = 51;
-            this.dtgridVehicles.RowTemplate.Height = 24;
-            this.dtgridVehicles.Size = new System.Drawing.Size(1034, 505);
-            this.dtgridVehicles.TabIndex = 0;
-            this.dtgridVehicles.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgridVehicles_CellContentClick);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(20, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Tìm kiếm";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(124, 17);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(608, 30);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(159, 69);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(141, 24);
-            this.comboBox1.TabIndex = 3;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(507, 69);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(141, 24);
-            this.comboBox2.TabIndex = 4;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(841, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 49);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.comboBox2);
-            this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Controls.Add(this.richTextBox1);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.dtgridVehicles);
-            this.panel1.Location = new System.Drawing.Point(103, 46);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1034, 661);
-            this.panel1.TabIndex = 2;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(0, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-=======
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // dteManu
@@ -816,35 +568,19 @@
             this.dteManu.Name = "dteManu";
             this.dteManu.Size = new System.Drawing.Size(134, 26);
             this.dteManu.TabIndex = 21;
->>>>>>> b7d2c3d3db28c03e14b1e2b6c92b28315ea84070
             // 
             // FCar_Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(1732, 753);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
-=======
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(1715, 1043);
             this.Controls.Add(this.groupBox1);
->>>>>>> b7d2c3d3db28c03e14b1e2b6c92b28315ea84070
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FCar_Admin";
             this.Text = "FCar_Admin";
             this.Load += new System.EventHandler(this.FCar_Admin_Load);
-<<<<<<< HEAD
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgridVehicles)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-=======
             this.groupBox1.ResumeLayout(false);
             this.groupBox6.ResumeLayout(false);
             this.groupBox6.PerformLayout();
@@ -857,36 +593,11 @@
             this.groupBox3.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
->>>>>>> b7d2c3d3db28c03e14b1e2b6c92b28315ea84070
             this.ResumeLayout(false);
 
         }
 
         #endregion
-<<<<<<< HEAD
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.RichTextBox txtYear;
-        private System.Windows.Forms.RichTextBox txtName;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RichTextBox txtManu;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox txtCode;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dtgridVehicles;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Button btnEdit;
-        private System.Windows.Forms.Button btnDelete;
-        private System.Windows.Forms.Button button3;
-=======
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
@@ -926,6 +637,5 @@
         private System.Windows.Forms.Label label11;
         private FontAwesome.Sharp.IconButton btxCancel;
         private MonthYearPicker dteManu;
->>>>>>> b7d2c3d3db28c03e14b1e2b6c92b28315ea84070
     }
 }
