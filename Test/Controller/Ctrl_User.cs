df0d5ab4f4b546e5a60491abc25e04fdfed4dd80
Phạm Hoàng Huân <89542925+Hazhaz1412@users.noreply.github.com>
2024-11-29 23:21:38 +0700

    Ctrl_User
    Add,Upd,getdata,Remove

--- Test/Controller/Ctrl_User.cs
+++ Test/Controller/Ctrl_User.cs
@@ -25,7 +25,35 @@ namespace Test.Controller
 
             return users.Cast<object>().ToList();
         }
-        
+        public void AddUser(User newUser)
+        {
+            CUltils.db.Users.Add(newUser);
+            CUltils.db.SaveChanges();
+        }
+
+        public void RemoveUser(string userId)
+        {
+            var user = CUltils.db.Users.SingleOrDefault(u => u.IDUser == userId);
+            if (user != null)
+            {
+                CUltils.db.Users.Remove(user);
+                CUltils.db.SaveChanges();
+            }
+        }
+
+        public void UpdateUser(User updatedUser)
+        {
+            var user = CUltils.db.Users.SingleOrDefault(u => u.IDUser == updatedUser.IDUser);
+            if (user != null)
+            {
+                user.Name = updatedUser.Name;
+                user.Gender = updatedUser.Gender;
+                user.PhoneNumber = updatedUser.PhoneNumber;
+                user.Address = updatedUser.Address;
+                user.IdentityCard = updatedUser.IdentityCard;
+                CUltils.db.SaveChanges();
+            }
+        }
 
     }
 }
